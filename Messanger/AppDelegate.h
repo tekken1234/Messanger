18a1b79c3a7a29421a5b28eb89868138473ff639
tekken1234 <ldh@ldhtekiMac.local>
2014-08-20 12:03:46 +0800

    Initial Commit

---
+++ Messanger/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Messanger
+//
+//  Created by ldh on 14/8/20.
+//  Copyright (c) 2014年 ldh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
